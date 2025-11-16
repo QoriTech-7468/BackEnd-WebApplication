83c6e1d401c82fea50399cb8c2bb0493b1f8ba1e
Jesús Castillo Vidal <jcdev2004@gmail.com>
2025-11-16 16:48:24 -0500

    refactor: Removing Appdbcontext for suscription

--- Rutana.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ Rutana.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -21,38 +21,7 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
 
-        // Subscriptions - Organization aggregate
-        builder.Entity<Organization>(entity =>
-        {
-            // Primary key
-            entity.HasKey(o => o.Id);
-
-            // Id configuration (similar to Profile.Id)
-            entity.Property(o => o.Id)
-                .IsRequired()
-                .HasConversion(
-                    id => id.Value,
-                    value => new OrganizationId(value))
-                .ValueGeneratedOnAdd();
-
-            // Name value object as owned type (let ModelBuilderExtensions convert to snake_case)
-            entity.OwnsOne(o => o.Name, n =>
-            {
-                n.WithOwner().HasForeignKey("Id");
-                n.Property(p => p.Value)
-                    .HasColumnName("Name")
-                    .HasMaxLength(200);
-            });
-
-            // Ruc value object as owned type (let ModelBuilderExtensions convert to snake_case)
-            entity.OwnsOne(o => o.Ruc, r =>
-            {
-                r.WithOwner().HasForeignKey("Id");
-                r.Property(p => p.Value)
-                    .HasColumnName("Ruc")
-                    .HasMaxLength(20);
-            });
-        });
+       
 
         builder.UseSnakeCaseNamingConvention();
     }
