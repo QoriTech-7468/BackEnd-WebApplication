677142b126be8a4cff162098cc1c2a9549071061
Yaku Guzman <yakumgc12@gmail.com>
2025-11-16 16:31:00 -0500

    feat(crm): Add commands for client and location management

---
+++ Rutana.API/CRM/Domain/Model/Commands/EnableClientCommand.cs
@@ -0,0 +1,7 @@
+namespace Rutana.API.CRM.Domain.Model.Commands;
+
+/// <summary>
+/// Command to enable a client.
+/// </summary>
+/// <param name="ClientId">The identifier of the client to enable.</param>
+public record EnableClientCommand(int ClientId);
