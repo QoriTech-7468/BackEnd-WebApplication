50a3a6c7c50f4e5e494623482e893d65f4cb04ff
Yaku Guzman <yakumgc12@gmail.com>
2025-11-16 11:48:21 -0500

    feat: Enhance Program.cs with database services, OpenAPI documentation, and shared context configuration

--- Rutana.API/Program.cs
+++ Rutana.API/Program.cs
@@ -1,23 +1,34 @@
+using Rutana.API.Shared.Infrastructure.Documentation.OpenApi.Configuration.Extensions;
+using Rutana.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using Rutana.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
+using Rutana.API.Shared.Infrastructure.Mediator.Cortex.Configuration.Extensions;
+using Rutana.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add Database Services
+builder.AddDatabaseServices();
+
+// Open API Configuration
+builder.AddOpenApiDocumentationServices();
+
+// Dependency Injection
+builder.AddSharedContextServices();
+
+// Mediator Configuration
+builder.AddCortexConfigurationServices();
 
 var app = builder.Build();
 
+// Verify if the database exists and create it if it doesn't
+app.UseDatabaseCreationAssurance();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.UseOpenApiDocumentation();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
