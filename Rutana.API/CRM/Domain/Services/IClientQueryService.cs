3e244ae9647651da9005b34fc3c03d4401bd3cda
Yaku Guzman <yakumgc12@gmail.com>
2025-11-16 16:44:52 -0500

    feat(crm): Add command and query services for managing clients and locations

---
+++ Rutana.API/CRM/Domain/Services/IClientQueryService.cs
@@ -0,0 +1,24 @@
+using Rutana.API.CRM.Domain.Model.Aggregates;
+using Rutana.API.CRM.Domain.Model.Queries;
+
+namespace Rutana.API.CRM.Domain.Services;
+
+/// <summary>
+/// Represents the client query service in the Rutana CRM System.
+/// </summary>
+public interface IClientQueryService
+{
+    /// <summary>
+    /// Handles the get clients by organization id query.
+    /// </summary>
+    /// <param name="query">The query.</param>
+    /// <returns>A collection of clients.</returns>
+    Task<IEnumerable<Client>> Handle(GetClientsByOrganizationIdQuery query);
+
+    /// <summary>
+    /// Handles the get client by id query.
+    /// </summary>
+    /// <param name="query">The query.</param>
+    /// <returns>The client if found, otherwise null.</returns>
+    Task<Client?> Handle(GetClientByIdQuery query);
+}
