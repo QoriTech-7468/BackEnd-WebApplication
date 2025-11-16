d07dc9b3eef0392caddf17724699d4869436c80a
Yaku Guzman <yakumgc12@gmail.com>
2025-11-16 17:52:59 -0500

    feat(crm): Add assemblers for converting ClientWithLocations and RegisterLocation resources

---
+++ Rutana.API/CRM/Interfaces/REST/Transform/ClientWithLocationsResourceAssembler.cs
@@ -0,0 +1,34 @@
+using Rutana.API.CRM.Domain.Model.Aggregates;
+using Rutana.API.CRM.Interfaces.REST.Resources;
+
+namespace Rutana.API.CRM.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert Client and Locations to ClientWithLocationsResource.
+/// </summary>
+public static class ClientWithLocationsResourceAssembler
+{
+    /// <summary>
+    /// Converts a Client entity and its locations to a ClientWithLocationsResource.
+    /// </summary>
+    /// <param name="client">The Client entity.</param>
+    /// <param name="locations">The collection of Location entities.</param>
+    /// <returns>The ClientWithLocationsResource.</returns>
+    public static ClientWithLocationsResource ToResourceFromEntity(Client client, IEnumerable<Location> locations)
+    {
+        var locationResources = locations.Select(location => 
+            new LocationResource(
+                location.Id,
+                location.Name.Value,
+                location.Proximity.ToString(),
+                location.IsEnabled,
+                ClientSummaryResourceFromEntityAssembler.ToResourceFromEntity(client)
+            ));
+
+        return new ClientWithLocationsResource(
+            client.Id,
+            client.CompanyName.Value,
+            client.IsEnabled,
+            locationResources);
+    }
+}
