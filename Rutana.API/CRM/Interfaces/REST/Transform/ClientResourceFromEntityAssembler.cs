e34fb89a88a794f312ccd6da595232ac409d1684
Yaku Guzman <yakumgc12@gmail.com>
2025-11-16 17:52:34 -0500

    feat(crm): Add assemblers for converting Client entities and resources

---
+++ Rutana.API/CRM/Interfaces/REST/Transform/ClientResourceFromEntityAssembler.cs
@@ -0,0 +1,23 @@
+using Rutana.API.CRM.Domain.Model.Aggregates;
+using Rutana.API.CRM.Interfaces.REST.Resources;
+
+namespace Rutana.API.CRM.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to convert Client entity to ClientResource.
+/// </summary>
+public static class ClientResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Converts a Client entity to a ClientResource.
+    /// </summary>
+    /// <param name="entity">The Client entity.</param>
+    /// <returns>The ClientResource.</returns>
+    public static ClientResource ToResourceFromEntity(Client entity)
+    {
+        return new ClientResource(
+            entity.Id,
+            entity.CompanyName.Value,
+            entity.IsEnabled);
+    }
+}
