02849d24e4168c3067377d0060df13ecd65a4c06
Jesús Castillo Vidal <jcdev2004@gmail.com>
2025-11-16 22:56:58 -0500

    fix: Refactoring ClientId and LocationId

--- Rutana.API/CRM/Application/Internal/CommandServices/ClientCommandService.cs
+++ Rutana.API/CRM/Application/Internal/CommandServices/ClientCommandService.cs
@@ -1,9 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Rutana.API.CRM.Domain.Model.Aggregates;
 using Rutana.API.CRM.Domain.Model.Commands;
 using Rutana.API.CRM.Domain.Model.ValueObjects;
 using Rutana.API.CRM.Domain.Repositories;
 using Rutana.API.CRM.Domain.Services;
 using Rutana.API.Shared.Domain.Repositories;
+using Rutana.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 namespace Rutana.API.CRM.Application.Internal.CommandServices;
 
@@ -12,9 +14,11 @@ namespace Rutana.API.CRM.Application.Internal.CommandServices;
 /// </summary>
 /// <param name="clientRepository">The client repository.</param>
 /// <param name="unitOfWork">The unit of work.</param>
+/// <param name="context">The database context.</param>
 public class ClientCommandService(
     IClientRepository clientRepository,
-    IUnitOfWork unitOfWork) : IClientCommandService
+    IUnitOfWork unitOfWork,
+    AppDbContext context) : IClientCommandService
 {
     /// <inheritdoc />
     public async Task<Client?> Handle(RegisterClientCommand command)
@@ -31,6 +35,12 @@ public class ClientCommandService(
         var client = new Client(command);
         await clientRepository.AddAsync(client);
         await unitOfWork.CompleteAsync();
+        
+        // After SaveChanges, update the ClientId with the generated value
+        // EF Core generates the ID in the database but doesn't update the value object automatically
+        // We need to reload the entity to get the generated ID value
+        await context.Entry(client).ReloadAsync();
+        
         return client;
     }
 
