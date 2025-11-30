c9d6c93d0b03c48046d6cb6f89e37c164193aa7a
miniChorri <christofercosta_8@hotmail.com>
2025-11-30 16:50:36 -0500

    feat(subscription):Added the Rest Resource and transform for Payment

---
+++ Rutana.API/Suscriptions/Interfaces/REST/Transform/PaymentResourceFromEntityAssembler.cs
@@ -0,0 +1,26 @@
+﻿using Rutana.API.Suscriptions.Domain.Model.Aggregates;
+using Rutana.API.Suscriptions.Interfaces.REST.Resources;
+
+namespace Rutana.API.Suscriptions.Interfaces.REST.Transform;
+
+/// <summary>
+///     Assembler to create a <see cref="PaymentResource" /> from an entity.
+/// </summary>
+public static class PaymentResourceFromEntityAssembler
+{
+    /// <summary>
+    ///     Create a <see cref="PaymentResource" /> from a Payment entity.
+    /// </summary>
+    /// <param name="entity">The <see cref="Payment" /> entity.</param>
+    /// <returns>The resource created from the entity.</returns>
+    public static PaymentResource ToResourceFromEntity(Payment entity)
+    {
+        return new PaymentResource(
+            entity.Id,
+            entity.Amount,
+            entity.Currency,
+            entity.Status,
+            entity.UserId
+        );
+    }
+}
