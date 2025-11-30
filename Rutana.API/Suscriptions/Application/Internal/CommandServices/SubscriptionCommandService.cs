3819bb9d1d5adf61a0b10a30ea2ee229d8befb98
SantiIHC <u202215160@upc.edu.pe>
2025-11-30 17:16:25 -0500

    feat(subscriptions): Implement subscription application query service and command service"

---
+++ Rutana.API/Suscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
@@ -0,0 +1,19 @@
+using Rutana.API.Shared.Domain.Repositories;
+using Rutana.API.Suscriptions.Domain.Model.Aggregates;
+using Rutana.API.Suscriptions.Domain.Model.Commands;
+using Rutana.API.Suscriptions.Domain.Repositories;
+using Rutana.API.Suscriptions.Domain.Services;
+
+namespace Rutana.API.Suscriptions.Application.Internal.CommandServices;
+
+public class SubscriptionCommandService(ISubscriptionRepository subscriptionRepository, IUnitOfWork unitOfWork) 
+    : ISubscriptionCommandService
+{
+    public async Task<Subscription> Handle(CreateSubscription command)
+    {
+        var subscription = Subscription.Create(command);
+        await subscriptionRepository.AddAsync(subscription);
+        await unitOfWork.CompleteAsync();
+        return subscription;
+    }
+}
